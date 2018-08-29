ed8ea48b0bc2a08c9d9199544ffe9bf41b1e3eb1
aginwala <aginwala@ebaysf.com>
2018-08-28 17:16:11 -0700

    updating comments for sqrt

--- sqrt.go
+++ sqrt.go
@@ -1,5 +1,5 @@
 // program to find square root of a number
-// go tutorial https://tour.golang.org/flowcontrol/8
+// Solves: go tutorial https://tour.golang.org/flowcontrol/8
 // run as: go run sqrt.go
 package main
 
@@ -10,19 +10,19 @@ import (
 
 func Sqrt(x float64) float64 {
     t, z := 0., 1.
-	for {
-	     z, t = z - (z * z-x ) / (2 * z), z
-		 fmt.Println("z is:", z)
-         if math.Abs(t-z) < 1e-8 {
+    for {
+        z, t = z - (z * z-x ) / (2 * z), z
+        fmt.Println("z is:", z)
+        if math.Abs(t-z) < 1e-8 {
             break
-         }
+        }
     }
     return z
 }
 
 func main() {
     i := 3.
-	sqrt_i := Sqrt(i)
+    sqrt_i := Sqrt(i)
     fmt.Println(sqrt_i)
     fmt.Println(sqrt_i == math.Sqrt(i))
 }
